9f56940a2850e9534abaf429a9cd17ca292e60cc
HimanshuChourasia <himanshuchourasia@gmail.com>
2019-06-13 15:03:32 -0400

    merged conflicts

--- Client/src/auth/Login.js
+++ Client/src/auth/Login.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
     render() {
         return (
-            <div>
-                <h4>Login</h4>
+            <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+      
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn btn-link">Sign In</button>
             </div>
+            <div>
+            <Link to="/register" className="btn btn-link">Sign Up</Link>
+            </div>
+        </form>
+      </div>
         );
     }
 }
 
 export default Login;
+
+
+
+
+
+
+
--- Client/src/auth/register.js
+++ Client/src/auth/register.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
 
 class Register extends Component {
+    state = {
+        email: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+      }
+      handleChange = (e) => {
+        this.setState({
+          [e.target.id]: e.target.value
+        })
+      }
+      handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+      }
     render() {
         return (
-            <div>
-                <h4>Register</h4>
-            </div>
+            <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id='firstName' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id='lastName' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn btn-link">Sign Up</button>
+          </div>
+        </form>
+      </div>
         );
     }
 }
